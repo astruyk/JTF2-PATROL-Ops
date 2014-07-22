40511af5860d03c33242ce4bce32ccc5906ba68a
anton <astruyk@gmail.com>
2014-07-22 08:39:43 -0400

    Remove .bak file if it exists
    We don't need to backup the PBO since it's being done via git. We can
    just clobber it so it doesn't show up in diffs.

--- scripts/Generate_PBO.bat
+++ scripts/Generate_PBO.bat
@@ -1,2 +1,3 @@
 ..\lib\PBOManager_v_1_4_beta\PBOConsole.exe -pack ..\mission ..\bin\JTF2_Patrol_Ops_NATO.Altis.pbo
+if exist ..\bin\\JTF2_Patrol_Ops_NATO.Altis.pbo.bak del ..\bin\\JTF2_Patrol_Ops_NATO.Altis.pbo.bak
 pause
