0109badf13529cf555cc20055024d42e97611169
anton <astruyk@gmail.com>
2014-07-26 22:35:47 -0400

    Remove old PBO. Update new one. Fix bat.
    Fix the batch file not deleting backup. Remove old PBO. Update to latest
    new one.

--- scripts/Generate_PBO.bat
+++ scripts/Generate_PBO.bat
@@ -1,3 +1,3 @@
 ..\lib\PBOManager_v_1_4_beta\PBOConsole.exe -pack ..\mission ..\bin\JTF2_Patrol_Ops_NATO_TFAR.Altis.pbo
-if exist ..\bin\\JTF2_Patrol_Ops_NATO.Altis.pbo.bak del ..\bin\\JTF2_Patrol_Ops_NATO_TFAR.Altis.pbo.bak
+if exist ..\bin\JTF2_Patrol_Ops_NATO_TFAR.Altis.pbo.bak del ..\bin\JTF2_Patrol_Ops_NATO_TFAR.Altis.pbo.bak
 pause
