088f0c8b66202e1b4c71d9704d9cd09fc6acf2d7
anton <astruyk@gmail.com>
2014-08-11 21:28:55 -0400

    Reduce chance of Kajmin spam
    There used to be a 50% chance that you'd get a kajmin when choosing a
    transport helo. And a 100% chance when asking for an attack helo.
    Changed to 50% change when asking for attack helo - 0% chance when
    spawning a transport helo.

--- mission/Patrol_Ops_3_Configuration.hpp
+++ mission/Patrol_Ops_3_Configuration.hpp
@@ -94,15 +94,15 @@ PO3_preDefinedEnemyVehicles = [
 
 /* CSAT Forces */
 /* 0 - Anti Air Vehicles */	 ["O_APC_Tracked_02_AA_F"]
-/* 1 - Attack Helos */		,["O_Heli_Attack_02_black_F","O_Heli_Attack_02_F"]
+/* 1 - Attack Helos */		,["O_Heli_Attack_02_black_F","O_Heli_Light_02_F"]
 /* 2 - Attack Planes */		,["O_UAV_02_F","O_UAV_02_CAS_F"]
 /* 3 - Heavy Vehicles */	,["O_MBT_02_cannon_F","O_APC_Tracked_02_cannon_F"]
 /* 4 - Light Vehicles */	,["O_MRAP_02_gmg_F","O_MRAP_02_hmg_F"]
 /* 5 - Medium Vehicles */	,["O_APC_Wheeled_02_rcws_F"]
 /* 6 - Mobile Artillery */	,["O_MBT_02_arty_F"]
-/* 7 - Transport Helos */	,["O_Heli_Attack_02_black_F","O_Heli_Attack_02_F","O_Heli_Light_02_F","O_Heli_Light_02_unarmed_F"]
+/* 7 - Transport Helos */	,["O_Heli_Light_02_F","O_Heli_Light_02_unarmed_F","O_Heli_Light_02_F","O_Heli_Light_02_unarmed_F"]
 /* 8 - Transport Planes */	,[]
-/* 9 - Transport Trucks */	,["O_Truck_02_covered_F","O_Truck_02_transport_F"]
+/* 9 - Transport Trucks */	,["O_Truck_02_covered_F","O_Truck_02_transport_F", "O_Truck_03_transport_F", "O_Truck_03_covered_F"]
 /*10 - Static Defence */	,["O_HMG_01_high_F","O_GMG_01_high_F","O_HMG_01_F","O_GMG_01_F","O_Mortar_01_F"]
 /*11 - Boats */				,["O_Boat_Armed_01_hmg_F"]
 /*12 - UAV */				,["O_UAV_02_CAS_F","O_UAV_02_F"]
